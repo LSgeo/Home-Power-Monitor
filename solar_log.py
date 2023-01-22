9967b19b79c56b3c8a70aab6c9072e96e650ee74
Luke Smith (Pi) <lukesmith.geo@gmail.com>
2023-01-22 11:15:54 +0800

    Refactor to functions and make nice plot

--- solar_log.py
+++ solar_log.py
@@ -1,76 +1,123 @@
-from datetime import datetime, timedelta
-import urllib.request, json, time
+import time
+import urllib.request, json
+from datetime import datetime
 from pathlib import Path
 
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
-from bokeh.plotting import figure
-from bokeh.resources import CDN
 from bokeh.embed import file_html
+from bokeh.io import output_notebook
+from bokeh.models import Range1d, DatetimeTickFormatter
+from bokeh.plotting import figure, show
+from bokeh.resources import CDN
+from bokeh.palettes import Category10
 
-server = "http://10.0.0.101/cm?cmnd=Status%208"  # IP of Enovy s energy meter
+output_notebook()
+
+server = "http://10.0.0.101/cm?cmnd=Status%208"  
 desk   = "http://10.0.0.102/cm?cmnd=Status%208"
-delay = 3  ## Seconds, delay between measurements
+delay = 60  ## Seconds, delay between measurements
 timer = 0
 first_run = True
 
 print("Program Running...")
 
-while True:
-    ## Getting data off Envoy S
-    # get html object, read it and decode it from byte to str
-    envoy_str = (urllib.request.urlopen(server).read().decode())
-    envoy_jsn = json.loads(envoy_str)
-    # print(json.dumps(jstruct, sort_keys=True, indent=4)) #Pretty Print the structure
-    s_cons_wnow = envoy_jsn["StatusSNS"]["ENERGY"]["Power"]
-    s_cons_day  = envoy_jsn["StatusSNS"]["ENERGY"]["Today"]  # Net power
+def get_data(addr, field):
+    """Get the contents of field from address"""
+    response = (urllib.request.urlopen(addr).read().decode())
+    jsn = json.loads(response)
+    # print(json.dumps(jstruct, sort_keys=True, indent=4)) #Pretty Print
+    return jsn["StatusSNS"]["ENERGY"][field]
 
-    desk_str = (urllib.request.urlopen(desk).read().decode())
-    desk_jsn = json.loads(desk_str)
-    # print(json.dumps(jstruct, sort_keys=True, indent=4)) #Pretty Print the structure
-    d_cons_wnow = desk_jsn["StatusSNS"]["ENERGY"]["Power"]
-    d_cons_day  = desk_jsn["StatusSNS"]["ENERGY"]["Today"]  # Net power
+def log_data(log_dir="logs"):
+    """Log data to log_dir, making new file each day"""
+    name_date = datetime.now().strftime("%d_%m_%Y")
+    log_path = Path(log_dir) / f"log_{name_date}.txt"
 
-    ## Print to Text File
-    name_date = time.strftime("%d_%m_%Y")
-    log_path = Path(f"logs/log_{name_date}.txt")
     if not log_path.exists():  # Make a new file for a new date
         newfile = open(log_path, "w")
-        newfile.write("Consumption_server (W), Consumption_desk (W), H:M:S\n")  # Header
+        newfile.write(
+            "Consumption_server (W),"
+            "Consumption_server_day (kWh),"
+            "Consumption_desk (W),"
+            "Consumption_desk_day (kWh),"
+            "Timestamp (ISO)"
+            "\n"
+            )  # Header
         newfile.close()
         print(f"Writing to new file: {log_path}")
-    text_num = f"{s_cons_wnow}, {d_cons_wnow}, {time.strftime('%H:%M:%S')}" 
+
+    text_num = (
+        f"{get_data(server, 'Power')},"
+        f"{get_data(server, 'Today')},"
+        f"{get_data(desk, 'Power')},"
+        f"{get_data(desk, 'Today')},"
+        f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}"        "\n" 
+        )
+
     txt_file = open(log_path, "a")
-    txt_file.write(f'{text_num}\n')
+    txt_file.write(text_num)
     txt_file.close()
 
-    time.sleep(delay)
+    return log_path
 
-    data = np.genfromtxt(
-        log_path,
-        delimiter=",",
-        skip_header=1,
-        names=["Consumption_s", "Consumption_d"],
-        usecols=(0, 1),
+def plot_log(log_path, html_path="plot.html"):
+    """Generate a html file containing an autorefreshing plot
+    of the data in log_path csv.
+    """
+
+    data = pd.read_csv(log_path)
+    data["Timestamp (ISO)"] = pd.to_datetime(data["Timestamp (ISO)"])
+    sensors = ["Server", "Desk"]
+    data_dict = {
+        "timestamps": data["Timestamp (ISO)"],
+        "Server": -data["Consumption_server (W)"],
+        "Desk": -data["Consumption_desk (W)"],
+    }
+
+    plot = figure(x_axis_type="datetime")
+    plot.height = 480 # Rpi Screen is 800*480
+    plot.width = 720  # 1440 minutes in day
+    
+    plot.vbar_stack(
+        sensors,
+        x="timestamps",
+        source=data_dict,
+        legend_label=sensors,
+        color=Category10[3][:2],
+        )
+
+    plot.y_range = Range1d(-500, 0) # 500 W display limit
+    # plot.x_range = times
+    plot.xaxis.formatter=DatetimeTickFormatter(
+        hours=["%d %B %Y"],
+        days=["%d %B %Y"],
+        months=["%d %B %Y"]
     )
 
-    timestamp = np.loadtxt(log_path, dtype="U", delimiter=",", skiprows=1, usecols=(2))
-    times = [datetime.strptime(t, " %H:%M:%S") for t in timestamp]
-    x = mpl.dates.date2num(times)
-    tfmt = mpl.dates.DateFormatter(" %H:%M:%S")
+    plot.xaxis.axis_label = "Time"
+    plot.yaxis.axis_label = "Watts"
 
-    plot = figure()
-    plot.vbar(x, top=-data["Consumption_s"], width=0.0002)
-    plot.vbar(x, top=-data["Consumption_d"], width=0.0002)
-    html = file_html(plot, CDN, "my plot")
+    plot.legend.location = "bottom_right"
+    plot.legend.orientation = "horizontal"
+
+    show(plot)
+
+    html = file_html(plot, CDN, "Power Consumption")
     html_list = html.split("\n")
     html_list.insert(10, '<meta http-equiv="refresh" content="10" >\n')
 
     html_new = "".join(html_list)
 
-    with open("derp.html", "w") as file1:
-        file1.write(html_new)
-        file1.close()
+    with open(html_path, "w") as f:
+        f.write(html_new)
+        f.close()
+
+    return html_path
+
+
+while True: 
+    log_path = log_data()
+    plot_log(log_path)
+
+    time.sleep(delay)
