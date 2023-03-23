cf7706680a6bb9960f930fa0214eb73f5380505c
Luke Smith (Pi) <lukesmith.geo@gmail.com>
2023-03-23 22:14:58 +0800

    Add kasa meter

--- solar_log.py
+++ solar_log.py
@@ -1,6 +1,7 @@
 #!/home/luke/Home-Power-Monitor/home/bin/python3
 
 import json
+from subprocess import Popen, PIPE
 import time
 import urllib.request
 from datetime import datetime, timedelta
@@ -9,43 +10,62 @@ from urllib.error import HTTPError, URLError
 
 import pandas as pd
 from bokeh.embed import file_html
-from bokeh.models import DatetimeTickFormatter, LinearAxis, Range1d  # , RangeTool
+from bokeh.models import DatetimeTickFormatter, LinearAxis, Range1d
 from bokeh.plotting import figure, show
 from bokeh.palettes import Category10
 from bokeh.resources import CDN
 
-# from bokeh.io import output_notebook, curdoc
+# from bokeh.io import output_notebook
 # output_notebook()
-# curdoc().theme = "dark_minimal"
 
 
 desk = "http://10.0.0.101/cm?cmnd=Status%208"
 server = "http://10.0.0.102/cm?cmnd=Status%208"
-delay = 60  ## Seconds, delay between measurements
+kasa = "10.0.0.59"
+
+price_per_kwh = 0.322957  # 2023 Synergy AUD/kWh
+delay = 61  ## Seconds, delay between measurements
 timer = 0
 first_run = True
 
 
-def get_data(addr, field):
+def get_data(addr: str, field: str) -> str:
     """Get the contents of field from address"""
     response = urllib.request.urlopen(addr).read().decode()
     jsn = json.loads(response)
     # print(json.dumps(jstruct, sort_keys=True, indent=4)) #Pretty Print
-    return jsn["StatusSNS"]["ENERGY"][field]
+    return str(jsn["StatusSNS"]["ENERGY"][field])
 
 
-def log_data(log_dir="logs"):
+def get_kasa_data(addr: str, field: str) -> str:
+    """Query Kasa device using some kasa tool from github"""
+    cmd = f"(/home/luke/.local/bin/kasa --host {addr} --type plug emeter)"
+    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    o, e = proc.communicate(timeout=10)
+
+    output = o.decode("ascii")
+    print(e.decode("ascii"))
+
+    p = output.split()[10]
+    t = output.split()[14]
+
+    return dict(Power=p, Total=t)[field]
+
+
+def log_data(log_dir: str = "logs") -> Path:
     """Log data to log_dir, making new file each month"""
-    name_date = "continuous"  # datetime.now().strftime("%m_%Y")
+    name_date = "continuous_kasa"  # datetime.now().strftime("%m_%Y")
     log_path = Path(log_dir) / f"log_{name_date}.txt"
 
     if not log_path.exists():  # Make a new file for a new date
         newfile = open(log_path, "w")
         newfile.write(
             "Consumption_server (W),"
-            "Consumption_server_day (kWh),"
+            "Consumption_server_total (kWh),"
             "Consumption_desk (W),"
-            "Consumption_desk_day (kWh),"
+            "Consumption_desk_total (kWh),"
+            "Consumption_kasa (W),"
+            "Consumption_kasa_total (kWh),"
             "Timestamp (ISO)"
             "\n"
         )  # Header
@@ -57,6 +77,8 @@ def log_data(log_dir="logs"):
         f"{get_data(server, 'Total')},"
         f"{get_data(desk, 'Power')},"
         f"{get_data(desk, 'Total')},"
+        f"{get_kasa_data(kasa, 'Power')},"
+        f"{get_kasa_data(kasa, 'Total')},"
         f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}"
         "\n"
     )
@@ -68,25 +90,27 @@ def log_data(log_dir="logs"):
     return log_path
 
 
-def plot_log(log_path, html_path="plot.html"):
+def plot_log(log_path: Path, html_path:str = "plot.html"):
     """Generate a html file containing an autorefreshing plot
     of the data in log_path csv.
     """
 
     data = pd.read_csv(log_path)
     data["Timestamp (ISO)"] = pd.to_datetime(data["Timestamp (ISO)"])
-    sensors = ["Server", "Desk"]
+    sensors = ["Server", "Desk", "Kasa"]
     data_dict = {
         "timestamps": data["Timestamp (ISO)"],
         "Server": -data["Consumption_server (W)"],
         "Desk": -data["Consumption_desk (W)"],
+        "Kasa": -data["Consumption_kasa (W)"],
         "Today": (
-            0.322957
+            price_per_kwh
             * (
-                -data["Consumption_desk_day (kWh)"]
-                - data["Consumption_server_day (kWh)"]
+                - data["Consumption_desk_total (kWh)"]
+                - data["Consumption_server_total (kWh)"]
+                - data["Consumption_kasa_total (kWh)"]
             )
-        ),  # synergy $/kWh
+        ),
     }
 
     # Rpi Screen is 800*480, but there is some padding
@@ -111,7 +135,7 @@ def plot_log(log_path, html_path="plot.html"):
         x="timestamps",
         width=timedelta(seconds=50),
         legend_label=sensors,
-        color=Category10[3][:2],
+        color=Category10[3],
     )
     plot.yaxis.axis_label = "Watts"
 
@@ -125,7 +149,7 @@ def plot_log(log_path, html_path="plot.html"):
         y_range_name="cumulative",
         legend_label="Cumulative",
         line_width=2.5,
-        color="#2CA02C",
+        color="#DDBBBB", #"#2CA02C",
     )
 
     # plot.x_range = times
